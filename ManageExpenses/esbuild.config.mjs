f231aec2a8ccead02a84872fa4a3459c619d7970
ukmukfukk <ukmukfukk@gmail.com>
2025-05-18 15:40:31 +0200

    Initial commit

---
+++ ManageExpenses/esbuild.config.mjs
@@ -0,0 +1,25 @@
+﻿import { existsSync, cpSync, readdirSync } from 'fs'
+import { build } from 'esbuild'
+
+if (existsSync('./build/Content/WebSharper/')) {
+  cpSync('./build/Content/WebSharper/', './wwwroot/Content/WebSharper/', { recursive: true });
+}
+
+const files = readdirSync('./build/Scripts/WebSharper/ManageExpenses/');
+
+files.forEach(file => {
+  if (file.endsWith('.js')) {
+    var options =
+    {
+      entryPoints: ['./build/Scripts/WebSharper/ManageExpenses/' + file],
+      bundle: true,
+      minify: true,
+      format: 'iife',
+      outfile: 'wwwroot/Scripts/WebSharper/' + file,
+      globalName: 'wsbundle'
+    };
+
+    console.log("Bundling:", file);
+    build(options);
+  }
+});
